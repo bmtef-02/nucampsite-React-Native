fb8b04ddb5e0f72f1ca1abc59ba6d28addc35b2e
Brian Jun <bmtef.02@gmail.com>
2021-11-08 22:42:45 -0800

    Components Part 1

---
+++ components/DirectoryComponent.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// These 2 components work well together - like <ul> and <li> in HTML
+import { FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+function Directory(props) {
+
+    // Flatlist by default passes an object to this function
+
+    // using {item} we can access the current item being iterated over
+    const renderDirectoryItem = ({item}) => {
+        return (
+            <ListItem
+                title={item.name}
+                subtitle={item.description}
+
+                // this prop requires an object - so have to use {{}}; outer {} is for JSX
+                // source is a property that we use to give an image location
+                leftAvatar={{ source: require('./images/react-lake.jpg')}}
+            />
+        );
+    };
+
+    return (
+
+        // Flatlist will iterate through every item in the array we gave to the data prop
+        // the function in the renderItem prop will run on every single item
+        <FlatList
+            // data property expects an array; holds the campsites array
+            data={props.campsites}
+
+            // this prop will specify how to render each item in the class
+            renderItem={renderDirectoryItem}
+
+            // this prop holds the unique id for each item in the campsites array
+            // similar to the key property in React
+            keyExtractor={item => item.id.toString()}
+        />
+    );
+}
+
+export default Directory;
