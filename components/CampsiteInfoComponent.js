4c0d0c6f326a117d122518438903a856df3d8ef5
Brian Jun <bmtef.02@gmail.com>
2021-11-19 23:50:04 -0800

    Redux Adding Favorites

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -3,12 +3,18 @@ import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
         campsites: state.campsites,
-        comments: state.comments
+        comments: state.comments,
+        favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {
+    postFavorite: campsiteId => (postFavorite(campsiteId))
 };
 
 function RenderCampsite(props) {
@@ -31,7 +37,7 @@ function RenderCampsite(props) {
                     color="#f50"
                     raised  // adds box shadow to icon
                     reverse // reverses color scheme
-                    onPress={() => props.markFavorite()}
+                    onPress={() => props.favorite ? console.log("Already set as a favorite") : props.markFavorite()}
                 />
             </Card>
         );
@@ -64,15 +70,8 @@ function RenderComments({comments}) {
 
 class CampsiteInfo extends Component {
     
-    constructor(props) {
-        super(props);
-        this.state = {
-            favorite: false
-        };
-    }
-    
-    markFavorite() {
-        this.setState({favorite: !this.state.favorite});
+    markFavorite(campsiteId) {
+        this.props.postFavorite(campsiteId);
     }
 
     // sets the title of the header
@@ -95,8 +94,8 @@ class CampsiteInfo extends Component {
         return (
             <ScrollView>
                 <RenderCampsite campsite={campsite} 
-                    favorite={this.state.favorite}
-                    markFavorite={() => this.markFavorite()}
+                    favorite={this.props.favorites.includes(campsiteId)}
+                    markFavorite={() => this.markFavorite(campsiteId)}
                 />
                 <RenderComments comments={comments} />
             </ScrollView>
@@ -105,4 +104,4 @@ class CampsiteInfo extends Component {
     
 }
 
-export default connect(mapStateToProps)(CampsiteInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
